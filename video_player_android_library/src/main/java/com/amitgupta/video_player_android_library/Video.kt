6b8cce4fd876b8b1fc11c48b9e46fc70950886fa
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-26 11:13:20 +0545

    [add][video_player]: adds Video and PlayType datastructures

---
+++ video_player_android_library/src/main/java/com/amitgupta/video_player_android_library/Video.kt
@@ -0,0 +1,8 @@
+package com.amitgupta.video_player_android_library
+
+data class Video(
+    var videoName: String = "",
+    var videoUrl: String = "http://www.streambox.fr/playlists/test_001/stream.m3u8",    //May be online url or local Uri
+    var videoPlayedDuration: Long = 0, //Will contain the millis of played video
+    var playType: PlayType
+)
